9bee32f9f1caf942b926457cf771e2d31f580f8c
Nara <narawit.tub@outlook.co.th>
2022-03-12 12:21:32 +0700

    bug: add bug to fail our test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Github Action 101" });
+  res.render("index", {
+    title: "Github Action 101",
+    message: "please accept 6 == 5",
+  });
 });
 
 module.exports = router;
